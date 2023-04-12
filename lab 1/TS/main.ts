1ffe255d8737ea2113a42edbd29462a7c62c7cf2
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-04-12 13:24:04 +0200

    add lab 1

---
+++ lab 1/TS/main.ts
@@ -0,0 +1,45 @@
+class Account{
+  Acc_no:number;
+  Balance:number;
+
+  debinAmount(){
+
+  }
+
+  creditAmount(){
+
+  }
+
+  getBalance(){
+
+  }
+}
+
+interface IAccount{
+  Date_of_opening:string;
+
+  addCustomer();
+  removeCustomer();
+}
+
+
+class Current_Account extends Account implements IAccount{
+  Date_of_opening: string;
+  Interest_rate:number;
+  addCustomer() {
+  }
+  removeCustomer() {
+  }
+
+}
+
+
+class Saving_Account extends Account implements IAccount{
+  Date_of_opening: string;
+  Min_Balance:number;
+  addCustomer() {
+  }
+  removeCustomer() {
+  }
+
+}
